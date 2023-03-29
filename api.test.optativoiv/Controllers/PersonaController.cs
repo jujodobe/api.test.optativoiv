355750ff0c85ca2ec9faa27af76fb1cc324d6128
Juan Dos Santos <jds.infosystem@gmail.com>
2023-03-29 19:30:30 -0400

    big bang

---
+++ api.test.optativoiv/Controllers/PersonaController.cs
@@ -0,0 +1,36 @@
+﻿using api.test.optativoiv.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.test.optativoiv.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonaController : Controller
+    {
+        private List<PersonaModel> personaModel;
+
+        public PersonaController()
+        {
+            personaModel = new List<PersonaModel>()
+            {
+                new PersonaModel { Id = 1, Nombre = "Juan", Apellido = "Perez", Edad = 30, Email = "jp@example.com", Telefono = "09812341564" },
+                new PersonaModel { Id = 2, Nombre = "Mario", Apellido = "Gomez", Edad = 25, Email = "mg@example.com", Telefono = "09812341545"},
+                new PersonaModel { Id = 3, Nombre = "Antonio", Apellido = "Mazacote", Edad = 25, Email = "am@example.com", Telefono = "09915674654"},
+            };
+        }
+
+        [HttpGet("ListarPersona")]
+        public ActionResult<List<PersonaModel>> ListarPersonas()
+        {
+            return Ok(personaModel);
+        }
+
+        [HttpGet("ConsultarPersona/{id}")]
+        public ActionResult<PersonaModel> ConsultarPersona(int id, string documento)
+        {
+            PersonaModel resultadoPersona = new PersonaModel();
+            return Ok(resultadoPersona);
+        }
+    }
+}
---
+++ api.test.optativoiv/Models/PersonaModel.cs
@@ -0,0 +1,18 @@
+﻿namespace api.test.optativoiv.Models
+{
+    public class PersonaModel
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public int Edad { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefono { get; set; }
+
+    }
+}
